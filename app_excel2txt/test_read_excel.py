459041b601882713a7b54120d10d3064f8563b58
Violeta Gotcheva <vgotcheva@flex5>
2018-12-30 22:49:39 -0500

    sample input

--- app_excel2txt/test_read_excel.py
+++ app_excel2txt/test_read_excel.py
@@ -2,19 +2,28 @@
 import pandas as pd
 
 # Assign spreadsheet filename to `file`
-mydir='app_excell2txt'
+mydir='app_excel2txt'
 myfile = 'test_input.xlsx'
 
 # Load spreadsheet
 xl = pd.ExcelFile(mydir + '\\' + myfile)
 
-# Print the sheet names
-print(xl.sheet_names)
+# Find the sheet names
+sheetnames = xl.sheet_names
 
-# Load a sheet into a DataFrame by name: df1
-df1 = xl.parse('Sheet1')
-print(df1)
-print("------")
-for row in df1.itertuples():
-    print(row)
-print(df1.iloc[1][1])
+# Loop over all sheets in the file
+for isheet in sheetnames:
+    # read data into a dataframe
+    #df1 = xl.parse(isheet)
+    df1= xl.parse(isheet, header=0)
+
+    # find dataframe size
+    myshape=df1.shape
+
+    #find column names
+    mycolumns=list(df1)
+# print(df1)
+# print("------")
+#  for row in df1.itertuples():
+#     print(row)
+# print(df1.iloc[1][1])
